5b191fbc364e7a9bfe09fc9417ce272672e6777f
KAVEESHA SITHUM <107849120+Kaveeshasithum@users.noreply.github.com>
2023-03-15 18:46:59 +0530

    🐼 DARK NERO V8 🐼

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = "cXNNbTFMWGE=" //session Id
+global.session = " " //session Id
 global.ownernomer = "94786802371" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94786802371'] //ur premium number
